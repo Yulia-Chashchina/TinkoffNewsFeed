8247203540ce932a2de4c67cb66c5d6424ff44ab
Юлия Чащина <uliacasina@macbook-pro-ulia.local>
2019-01-07 16:49:09 +0300

    Initial Commit

---
+++ TinkoffNewsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinkoffNewsFeed
+//
+//  Created by Юлия Чащина on 07/01/2019.
+//  Copyright © 2019 Yulia Chashchina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
